d9de60cada27ee17313dacab20f151f49f596427
Michel Boto <mike@Michel-Botos-MacBook-Pro.local>
2010-08-11 20:02:42 +0200

    added help option

--- Backend/main.cpp
+++ Backend/main.cpp
@@ -11,11 +11,12 @@ namespace {
   Options getOptionsFromCommandLine(const int argc, char* const argv[]) {
     Options o;
     namespace po = boost::program_options;
-    po::options_description od("Valid args");
-    od.add_options()(
-		     "port,p", 
-		     po::type_value<size_t>(&o.port)->default_value(DEFAULT_PORT), 
-		     "what port the backend should listen on");
+    po::options_description od("Server options");
+    od.add_options()
+      ("help,h", "displays this help message")
+      ("port,p", 
+       po::type_value<size_t>(&o.port)->default_value(DEFAULT_PORT), 
+       "what port the backend should listen on");
     po::variables_map vm;
     po::store(
 	      po::parse_command_line(
