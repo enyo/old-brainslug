e779ce256d6b4c51eda8d92813d32dee3b868053
Michel Boto <mike@Michel-Botos-MacBook-Pro.local>
2010-08-12 19:26:13 +0200

    added class decl for frontendconnection

--- Backend/FrontendConnection.h
+++ Backend/FrontendConnection.h
@@ -1,4 +1,16 @@
 #pragma once
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
 
-class FrontendConnection {
+class FrontendConnection : public boost::enable_shared_from_this<FrontendConnection> {
+public:
+  static boost::shared_ptr<FrontendConnection> create(boost::asio::io_service& ios);
+  boost::asio::ip::tcp::socket& socket();
+  void start();
+
+private:
+  FrontendConnection(boost::asio::io_service& ios);
+  void handle_write(const boost::system::error_code& error, size_t bytesTransferred);
+  
+  boost::asio::ip::tcp::socket _socket;
 };
