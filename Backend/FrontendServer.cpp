50e24697274c4fabe997d801cfbaef7866043e71
Michel Boto <mike@Michel-Botos-MacBook-Pro.local>
2010-08-12 22:44:26 +0200

    added handler for unregistered requests (mostly useful just for logging at the moment

--- Backend/FrontendServer.cpp
+++ Backend/FrontendServer.cpp
@@ -1,12 +1,21 @@
 #include "FrontendServer.h"
-#include "FrontendConnection.h"
+#include <boost/bind.hpp>
+#include <iostream>
 
 FrontendServer::FrontendServer(const size_t port)
   : _httpServer(boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)) {
+  _httpServer.setNotFoundHandler(
+				 boost::bind(&FrontendServer::handleNotFound, this, _1, _2)
+				 );
 }
 
 void FrontendServer::run() {
   if (!_httpServer.isListening())
     _httpServer.start();
   _httpServer.join();
+}
+
+void FrontendServer::handleNotFound(pion::net::HTTPRequestPtr& request, pion::net::TCPConnectionPtr& connection) {
+  std::cout << "Handled request with no registered service." << std::endl;
+  std::cout << "Request: [method=\"" << request->getMethod() << "\"] [resource=\"" << request->getOriginalResource() << "\"] [query=\"" << request->getQueryString() << "\"]" << std::endl;
 }
--- Backend/FrontendServer.h
+++ Backend/FrontendServer.h
@@ -1,12 +1,11 @@
 #pragma once
 #include <pion/net/HTTPServer.hpp>
 
-class FrontendConnection;
-
 class FrontendServer {
 public:
   FrontendServer(const size_t port);
   void run();
 private:
+  void handleNotFound(pion::net::HTTPRequestPtr&,pion::net::TCPConnectionPtr&);
   pion::net::HTTPServer _httpServer;
 };
