91dae5d154ebf6e7d32ff7f7757a50ae960cbf73
Michel Boto <mike@Michel-Botos-MacBook-Pro.local>
2010-08-14 23:23:31 +0200

    added test db for tv shows. mostly just copy paste from movies. really need to push all the duped code from the test dbs and the resource handlers into base classes

--- Backend/TVShowsResourceHandler.h
+++ Backend/TVShowsResourceHandler.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <pion/net/HTTPServer.hpp>
+#include "DB.h"
 
 class TVShowsResourceHandler {
 public:
+  TVShowsResourceHandler(const DBPtr db);
   void handle(pion::net::HTTPRequestPtr&,pion::net::TCPConnectionPtr&);
 private:
   void listTVShows(pion::net::HTTPRequestPtr&,pion::net::TCPConnectionPtr&);
   void findTVShowByID(pion::net::HTTPRequestPtr&,pion::net::TCPConnectionPtr&);
+
+  const DBPtr _db;
 };
